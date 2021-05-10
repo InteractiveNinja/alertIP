b7b1e0384c62812167bc5bd220ecc251abcc759a
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-05-10 11:45:25 +0200

    main Module procedure

--- ipalert.py
+++ ipalert.py
@@ -1,12 +1,20 @@
 
 
-from py.logger import Logger,logType
+import time
+from sys import argv
+from py.logger import Logger
 from py.configReader import Reader
-
+from py.check import Check
+from py.argParser import passArgs
 if(__name__ == "__main__"):
-    logger = Logger()
-    # checker = Check(logger)
-    Reader(logger)
+    noLog = passArgs(argv)
+    logger = Logger(noLog)
+    reader = Reader(logger)
+    checker = Check(logger,reader)
+    checkTime = reader.readConfig()["checktime"]
+    while(True):
+        checker.checkIP()
+        time.sleep(checkTime)
 
 
 
