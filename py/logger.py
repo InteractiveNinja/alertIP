088c6789219b5b8559f948fb797bc0b46e099e23
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-05-10 11:39:08 +0200

    for the argument parser added --no-log argument in logger

--- py/logger.py
+++ py/logger.py
@@ -10,18 +10,22 @@ class logType(Enum):
     error   = 4
 class Logger:
 
-    def __init__(self):
-        self.logpath = "{}\logs".format(getcwd())
-        if not path.isdir(self.logpath):
-            mkdir("logs")
-        self.logname = "{}.log".format(datetime.now().strftime("%H-%M-%S"))
-        self.logfile = open("{}\{}".format(self.logpath,self.logname),"w",encoding="utf-8")
-        self.logfile.write("--------- Begin of Log ---------\n")
-       
+    def __init__(self,noLog: bool):
+        self.__noLog = noLog
+        if noLog : print("File Logging is with --no-log disabled")
+        if not noLog:
+            self.__logpath = "{}\logs".format(getcwd())
+            if not path.isdir(self.__logpath):
+                mkdir("logs")
+            self.__logname = "{}.log".format(datetime.now().strftime("%H-%M-%S"))
+            self.__logfile = open("{}\{}".format(self.__logpath,self.__logname),"w",encoding="utf-8")
+            self.__logfile.write("--------- Begin of Log ---------\n")
+            
 
     def log(self,msg:str,logType : logType):
         time = datetime.now().strftime("%H:%M:%S")
         msgStr = "[{}] {}: {}\n".format(logType.name,time,msg)
-        self.logfile.write(msgStr)
+        if not self.__noLog:
+            self.__logfile.write(msgStr)
         print(msgStr)
 
